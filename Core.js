32ce40814c95216ee1c356246b76c9c612d46557
Balogh Zoltán Sándor <zoltan.balogh@avolantis.com>
2022-01-07 20:01:39 +0100

    feat: fix

--- Core.js
+++ Core.js
@@ -1,24 +1,5 @@
-class Version {
-    latest: boolean
-    version: string
-    updatePatchUrl: string
-    updaterUrl: string
-    changeLog: string[]
-}
 
-class Versions {
-    versions: Version[]
-}
 
 export class Core {
-    GetMechanic() {
-        let versions: Versions = null;
-        let response = fetch("https://gist.githubusercontent.com/zozobalogh0817/3b2ef4e635f0e3b7a9a3ffe3c0a5d051/raw")
-        response
-            .then(value => value.json())
-            .then(json => versions = json)
 
-        let sortedList = versions.versions.map(version => version.version)
-        console.log(sortedList);
-    }
 }
